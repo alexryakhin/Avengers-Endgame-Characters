508b16f4377acf8e326daff56f6bed3d0a76627c
Alex Bonney <bonney977@gmail.com>
2021-07-17 06:47:47 +0300

    Initial Commit

---
+++ DynamicGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicGrid
+//
+//  Created by Alexander Bonney on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DynamicGrid/DynamicGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGridApp.swift
+//  DynamicGrid
+//
+//  Created by Alexander Bonney on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
